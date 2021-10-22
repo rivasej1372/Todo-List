dd006126e9fc5dcfe95eecff3b67caf485ae97bd
rivasej1372 <rivasej@gmail.com>
2021-10-22 20:50:37 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+import React, { useState } from "react";
+export default function TodoList() {
+    const [inputItem, setInputItem] = useState("");
+    const [todos, setTodos] = useState(["Thing 1", "Thing 2", "Thing 3"]);
+    function addToTodos(e) {
+        e.preventDefault();
+        const updatedTodos = todos.concat(inputItem);
+        setTodos(updatedTodos);
+        console.log(todos);
+    }
+    function removeFromTodos(e) {
+        console.log("remove");
+    }
+    console.log("these are the todos" + todos);
+    return (
+        <div className="todoList">
+            <h1>Todo List</h1>
+            <input
+                value={inputItem}
+                onChange={e => setInputItem(e.target.value)}
+            />
+            <button onClick={addToTodos}>Submit</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default ;
